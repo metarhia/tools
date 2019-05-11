40d0156c284aa7a1ee3d5ff76cb0555402afd4b2
JuliaGerasymenko <gerasymenko.j.ke@gmail.com>
2019-05-11 11:14:10 +0300

    fix: support squash-all, cherry-pick, rebase

--- bin/land.js
+++ bin/land.js
@@ -5,27 +5,64 @@
 const childProcess = require('child_process');
 const https = require('https');
 
-const landedBranch = process.argv.slice(2);
+const arg = process.argv.slice(2);
+const landedBranch = arg[0];
 
-const options = {
-  headers: {
-    'User-Agent': 'request',
-  },
-  host: 'api.github.com',
-};
+const differCommits = () =>
+  parseInt(childProcess.execSync(`git rev-list --count master..HEAD`));
 
 const supportCommits = () => {
-  const differCommits = parseInt(
-    childProcess
-      .execSync(`git rev-list --count master..${landedBranch}`)
-      .toString('utf8')
-  );
-  return childProcess.execSync(
-    `git rebase --autosquash HEAD@{${differCommits}}`
-  );
+  const git = childProcess.spawn('git', [
+    'rebase',
+    '-i',
+    '--autosquash',
+    `HEAD~10`,
+  ]);
+  process.stdin.setRawMode(true);
+  process.stdin.pipe(git.stdin);
+  git.stdout.pipe(process.stdout);
+  git.stdout.on('close', code => {
+    if (code !== 0) {
+      process.exit(code);
+    }
+  });
 };
 
-const httpGet = commitMessage => {
+const supportRebase = () => {
+  const git = childProcess.spawn('git', [
+    'rebase',
+    '-i',
+    `HEAD~${differCommits()}`,
+  ]);
+  process.stdin.setRawMode(true);
+  process.stdin.pipe(git.stdin);
+  git.stdout.pipe(process.stdout);
+  git.stdout.on('close', code => {
+    if (code !== 0) {
+      process.exit(code);
+    }
+  });
+};
+
+const supportSquashAll = () => {
+  childProcess.execSync(`git reset --soft HEAD~${differCommits()}`);
+  const git = childProcess.spawn('git', ['commit']);
+  process.stdin.setRawMode(true);
+  process.stdin.pipe(git.stdin);
+  git.stdout.pipe(process.stdout);
+  git.stdout.on('close', code => {
+    if (code !== 0) {
+      process.exit(code);
+    }
+  });
+};
+
+const cherryPick = commitsList => {
+  childProcess.execSync(`git checkout ${landedBranch}`);
+  childProcess.execSync(`git cherry-pick ${commitsList}`);
+};
+
+const httpsGet = (commitMessage, options) => {
   https
     .get(options, res => {
       let data = '';
@@ -36,18 +73,14 @@ const httpGet = commitMessage => {
       });
 
       res.on('end', () => {
-        const primaryIndexPR = data.indexOf('html_url') + 11;
-        const prUrl = data.slice(
-          primaryIndexPR,
-          data.indexOf('"', primaryIndexPR)
-        );
+        const prUrl = JSON.parse(data).items[0].html_url;
+
         const extendedCommit = `${commitMessage}\n\nPR-URL: ${prUrl}`;
         childProcess.execSync(
           `git commit --amend --message='${extendedCommit}'`
         );
       });
     })
-
     .on('error', err => {
       console.error(err);
       process.exit(1);
@@ -55,24 +88,43 @@ const httpGet = commitMessage => {
 };
 
 const getPRURL = () => {
-  const gitConfig = childProcess
-    .execSync('git config --get remote.origin.url')
-    .toString('utf8');
-  supportCommits();
+  const gitConfig = childProcess.execSync('git config --get remote.origin.url');
+
+  if (arg.includes('--rebase')) supportRebase();
+
+  if (arg.includes('--fix-squash')) supportCommits();
+
+  if (arg.includes('--squash-all')) supportSquashAll();
 
   const repoName = gitConfig.slice(
     gitConfig.indexOf(':') + 1,
-    gitConfig.lastIndexOf('.')
+    gitConfig.lastIndexOf('.git')
   );
 
   const gitLog = childProcess
-    .execSync('git log -1 --pretty=format:%s')
+    .execSync('git log -1 --pretty=format:%B')
     .toString('utf8');
+  if (!gitLog.includes('PR-URL')) {
+    const options = {
+      path: `/search/issues?q=repo:${repoName}+is:pr+head:${landedBranch}`,
+      headers: {
+        'User-Agent': 'request',
+      },
+      host: 'api.github.com',
+    };
 
-  const commitMessage = gitLog.toString('utf8');
-  if (!commitMessage.includes('PR-URL')) {
-    options.path = `/search/issues?q=repo:${repoName}+is:pr+head:${landedBranch}`;
-    httpGet(commitMessage);
+    httpsGet(gitLog, options);
+  }
+
+  if (arg.includes('--cherry-pick')) {
+    const commitsList = childProcess
+      .execSync(`git log -${differCommits()} --pretty=format:%h`)
+      .toString('utf8')
+      .split('\n')
+      .reverse()
+      .join(' ');
+
+    cherryPick(commitsList);
   }
 };
 
